c9eca25c05cbc57a4ac006a4acd8e804ca6757c7
yelena guliyeva <gulbadam@gulbadam.com>
2018-11-01 18:31:26 -0700

    initial

---
+++ plinko/score.js
@@ -0,0 +1,10 @@
+const outputs = [];
+function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
+  // Ran every time a balls drops into a bucket
+  outputs.push([])
+}
+
+function runAnalysis() {
+  // Write code here to analyze stuff
+}
+
