665ad949f16ee211bd3d41a1b2467eb6bf25d13d
yelena guliyeva <gulbadam@gulbadam.com>
2018-11-08 19:04:55 -0800

    min max

--- plinko/score.js
+++ plinko/score.js
@@ -12,7 +12,7 @@ function runAnalysis() {
 const [testSet, trainingSet]= splitDataset(outputs, testSetSize);
 _.range(1, 15).forEach(k => {
 const accuracy = _.chain(testSet)
-.filter(testPoint => knn(trainingSet, testPoint[0], k) === testPoint[3])
+.filter(testPoint => knn(trainingSet, _.initial(testPoint), k) === testPoint[3])
 .size()
 .divide(testSetSize)
 .value();
@@ -28,7 +28,9 @@ function distance(pointA, pointB) {
 }
 function knn(data, point, k) {
  return  _.chain(data)
-    .map(row => [distance(row[0], point), row[3]])
+    .map(row => {
+      return [distance(_.initial(row), point), _.last(row)]
+    })
     .sortBy(row => row[0])
     .slice(0, k)
     .countBy(row => row[1])
@@ -44,4 +46,17 @@ const shuffled = _.shuffle(data);
 const testSet = _.slice(shuffled, 0, testCount);
 const trainingSet = _.slice(shuffled, testCount);
 return [testSet, trainingSet];
+}
+function minMax(data, featureCount) {
+  const clonedData = _.cloneDeep(data);
+  for (let i = 0; i < featureCount; i++) {
+    const column = clonedData.map(row=>row[i]);
+    const min =_.min(column);
+    const max =_.max(column);
+    for (let j = 0; j < clonedData.length; j++) {
+      clonedData[j][i] =(clonedData[j][i]-min)/ (max-min);
+      
+    }
+  }
+  return clonedData;
 }
